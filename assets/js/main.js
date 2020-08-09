ebb5d61ec54297ac82ed46b788935e552db638ec
steakandbake <steakandbake@protonmail.com>
2020-08-09 15:46:36 -0700

    Oh K

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,7 +25,7 @@ function numberWithCommas(n) {
 console.log("Fetching data...");
 getJSON("https://js.adapools.org/pools/f76e3a1104a9d816a67d5826a155c9e2979a839d0d944346d47e33ab/summary.json").then(data => {
   console.log(data);
-  var freeSpace = (((31112483745) / 100) - (data.data.total_stake / 1000000)).toFixed(0);
+  var freeSpace = (((31112483745) / 150) - (data.data.total_stake / 1000000)).toFixed(0);
   console.log(freeSpace);
   if (freeSpace > 100) {
   	freeSpace = numberWithCommas(freeSpace)
